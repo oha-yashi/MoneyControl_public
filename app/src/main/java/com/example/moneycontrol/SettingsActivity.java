f2b85f959ae00ca1bc902c8c8a3f46d03b588e5e
ohayashi2490 <ohayashi2490@gmail.com>
2021-02-23 13:32:04 +0900

    SettingsActivity.java 建設中ダイアログ設定 ラムダ式で記述

--- app/src/main/java/com/example/moneycontrol/SettingsActivity.java
+++ app/src/main/java/com/example/moneycontrol/SettingsActivity.java
@@ -11,11 +11,15 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -45,22 +49,34 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
             findPreference("show_all_button").setOnPreferenceClickListener(this);
+            findPreference("delete").setOnPreferenceClickListener((preference)->{
+                AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+                builder.setTitle("建設中").setMessage("少々お待ちください m(_ _)m").show();
+                return false;
+            });
             //ここでpreferenceのsetOnClickする
         }
 
         @Override
         public boolean onPreferenceClick(Preference preference) {
-            switch(preference.getKey()){
+            String key = preference.getKey();
+            Log.d("key is", key);
+            switch(key){
                 //onClickの内容はここで分岐する
                 case "show_all_button" :
-                    Log.d("showAll", "button clicked");
                     Intent intent = new Intent(getContext(), setting_showall.class);
                     startActivity(intent);
-                    break;
+                break;
+
+                /* //setOnClickListnerに直接記述
+                case "delete" :
+                    AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+                    builder.setTitle("建設中").setMessage("少々お待ちください m(_ _)m").show();
+                break;
+
+                 */
             }
             return false;
         }
     }
-
-
 }
