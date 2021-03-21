df324a203a3d093bb38592f84a04203c27006273
ohayashi2490 <ohayashi2490@gmail.com>
2021-03-21 11:07:10 +0900

    Ver 1.4.3 全体表示修正 new TextViewに動的にmarginをつけたい

--- app/src/main/java/com/example/moneycontrol/setting_showall.java
+++ app/src/main/java/com/example/moneycontrol/setting_showall.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.ViewGroup;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -31,14 +35,31 @@ public class setting_showall extends AppCompatActivity {
         for(int i=0; i<c.getCount(); i++){
             TableRow tr = new TableRow(this);
 
-            for(int j=1; j<5; j++){
+            for(int j=1; j<=5; j++){
                 tv = new TextView(this);
                 tv.setText(c.getString(j));
+                /*
+                try {
+                    int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
+                    ViewGroup.LayoutParams LP = new ViewGroup.LayoutParams(WC, WC);
+                    ViewGroup.MarginLayoutParams MLP = new ViewGroup.MarginLayoutParams(WC, WC);
+                    MLP.setMargins(0, 0, 2, 0);
+                    tv.setLayoutParams(MLP);
+                }catch (Exception e){
+                    Log.d("setMarginError", e.toString());
+                }
+                //*/
+
+                try{
+                    tv.setGravity(Gravity.END);
+                }catch (Exception e){
+                    Log.d("setGravityError", e.toString());
+                }
                 tr.addView(tv);
             }
 
             tv = new TextView(this);
-            String strNote = (c.getString(5).isEmpty() ? "" : c.getString(5) + " : ") + c.getString(6);
+            String strNote = (c.getString(6).isEmpty() ? "" : c.getString(6) + " : ") + c.getString(7);
             tv.setText(strNote);
             tr.addView(tv);
 
